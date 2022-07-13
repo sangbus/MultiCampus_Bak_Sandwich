c1ada3512621ac2fe3682f3161068acd7820b5bc
SangHwan <tkdghks58@gmail.com>
2022-07-13 15:18:00 +0900

    점수입력 0~100 학년입력 1~4 60점 이상 합격 미만 불합격 4학년은 70이상 합격

---
+++ Begin/src/multicampus/aaa.java
@@ -0,0 +1,30 @@
+package multicampus;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class aaa {
+	public static void main(String[] args) throws IOException
+			{
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				System.out.println("점수 입력해주세요(0~100) : ");
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				int score = Integer.parseInt(st.nextToken());
+				System.out.println("학년 입력해주세요(1~4) : ");
+				st = new StringTokenizer(br.readLine());
+				int num = Integer.parseInt(st.nextToken());
+				if(num==4 && score>=70)
+				{
+					System.out.println("4학년 합격입니다.");
+				}
+				else if(score>=60)
+				{
+					System.out.println("합격입니다.");
+				}
+				else
+				{
+					System.out.println("불합격입니다.");
+				}
+			}
+}
