5a62ece84f81fe1ad8d30d88ae999bc3842a9a77
SangHwan <tkdghks58@gmail.com>
2022-07-13 17:13:20 +0900

    수정

--- Begin/src/multicampus/number.java
+++ Begin/src/multicampus/number.java
@@ -7,10 +7,7 @@ public class number {
 				for(int i=1;i<=100;i++)
 				{
 					if(i%4==0||i%3==0)
-					{
 						sum+=i;
-						System.out.println(i);
-					}
 				}
 				System.out.println(sum);
 			}
