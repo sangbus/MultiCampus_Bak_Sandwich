632b940c7ed0a29ede2b2858460cec6821263299
SangHwan <tkdghks4564@naver.com>
2022-08-05 12:50:51 +0900

    2562번 문제

--- Bak/src/num_1541.java
+++ Bak/src/num_1541.java
@@ -6,22 +6,22 @@ public class num_1541 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int sum = Integer.MAX_VALUE;
-		String[] a = br.readLine().split("-");
+		int sum = Integer.MAX_VALUE; // 첫번째 수는 양수임을 체크하기 위한 값 설정
+		String[] a = br.readLine().split("-");	//입력받은 문자열에서 -를 분리한다.
 		for(int i=0;i<a.length;i++)
 		{
-			int temp=0;
-			String[] b = a[i].split("\\+");
+			int temp=0;	// 덧셈인 묶음의 합을 구하기위한 변수
+			String[] b = a[i].split("\\+");	// 덧셈 묶음인 수를 덧셈으로 다시 분리하여 합을 구한다.
 			
-			for(int j=0;j<b.length;j++)
+			for(int j=0;j<b.length;j++)	// 덧셈으로 나뉜 수들을 모두 더한다.
 				temp+=Integer.parseInt(b[j]);
 			
 			
-			if(sum==Integer.MAX_VALUE)
+			if(sum==Integer.MAX_VALUE)	// 첫번째 수일 경우 sum, temp값이 첫번째 수가 된다.
 				sum = temp;
 			
 			else
-				sum -= temp;
+				sum -= temp;	// 덧셈이 끝나면 뺄셈을 진행한다.
 		}
 		System.out.println(sum);
 	}
