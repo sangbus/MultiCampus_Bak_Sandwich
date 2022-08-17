91a827890247d2a3264e9e166d4fe8e90d6386b9
SangHwan <tkdghks4564@naver.com>
2022-08-17 15:31:53 +0900

    2884

---
+++ Bak/src/num_2884.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class num_2884 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int hour = Integer.parseInt(st.nextToken());
+		int minute = Integer.parseInt(st.nextToken());
+		int chang = minute - 45;
+		if(chang<0)
+		{
+			minute += 60;
+			hour--;
+			chang = minute - 45;
+			if(hour<0)
+			{
+				hour=23;
+			}
+		}
+		System.out.println(hour + " "+ chang);
+	}
+}
