33e73951a9a6a1c400163934c6d67f86522a2852
SangHwan <tkdghks4564@naver.com>
2022-08-28 21:41:04 +0900

    10828

---
+++ Bak/src/num_10828.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class num_10828 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int num = Integer.parseInt(br.readLine());
+		ArrayList<Integer> queue = new ArrayList<Integer>();
+		for(int i=0;i<num;i++)
+		{
+			int pushNum=0;
+			int popIndex = queue.size() - 1;
+			st=new StringTokenizer(br.readLine());
+			String Command = st.nextToken();
+			
+			if(st.hasMoreTokens()==true)
+			{
+				pushNum = Integer.parseInt(st.nextToken());
+			}
+			switch(Command) {
+			
+				case "push" :
+						queue.add(pushNum);
+					break;
+					
+				case "pop" :
+						if(popIndex<0)
+						{
+							sb.append(-1+"\n");
+						}
+						else
+						{
+							sb.append(queue.get(popIndex)+"\n");
+							queue.remove(popIndex);
+						}
+					break;
+					
+				case "size" :
+						sb.append(queue.size()+"\n");
+					break;
+					
+				case "empty" :
+						if(popIndex<0)
+						{
+							sb.append(1+"\n");
+						}
+						else
+						{
+							sb.append(0+"\n");
+						}
+					break;
+					
+				case "top" :
+						if(popIndex<0)
+						{
+							sb.append(-1+"\n");
+						}
+						else
+						{
+							sb.append(queue.get(popIndex)+"\n");
+						}
+					break;
+			}
+		}
+		System.out.println(sb);
+	}
+
+}
