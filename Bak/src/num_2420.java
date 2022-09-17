cc4f499bebca887d6ec23d0b75f24335b098a1a4
SangHwan <tkdghks4564@naver.com>
2022-09-17 22:16:09 +0900

    2420

---
+++ Bak/src/num_2420.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class num_2420 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		long num1=Long.parseLong(st.nextToken());
+		long num2=Long.parseLong(st.nextToken());
+		System.out.println(Math.abs(num1-num2));
+	}
+
+}
