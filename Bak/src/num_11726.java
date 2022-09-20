68ff11f834655282d56092123ad4cf8ae8f137f3
SangHwan <tkdghks4564@naver.com>
2022-09-20 19:40:09 +0900

    2579

--- Bak/src/num_11726.java
+++ Bak/src/num_11726.java
@@ -2,29 +2,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class num_2579 {
+public class num_11726 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int stair[] = new int[N+1];
-        int []dp = new int[N+1];
-        for(int i=1;i<=N;i++)
-        {
-            stair[i] = Integer.parseInt(br.readLine());
-        }
 
-        dp[1] = stair[1];
+        int dp[] = new int[N+1];
 
-        if(N >= 2)
-        {
-            dp[2] = stair[1]+stair[2];
-        }
-
+        dp[1] = 1;
+        dp[2] = 2;
 
         for(int i=3;i<=N;i++)
         {
-            dp[i] = Math.max(dp[i-3]+stair[i-1],dp[i-2])+stair[i];
+            dp[i]=(dp[i-1]+dp[i-2])%10007;
         }
+
         System.out.println(dp[N]);
     }
+
 }
