da82a6d91b22681149994407d20aa8db4763a9bb
SangHwan <tkdghks4564@naver.com>
2022-09-30 15:27:28 +0900

    15650

---
+++ Bak/src/num_15650.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class num_15650 {
+    static int arr[];
+    static StringBuilder sb;
+    static int M;
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+        dfs(1,0);
+        System.out.println(sb);
+    }
+
+    public static void dfs(int at,int depth)
+    {
+        if(depth==M)
+        {
+            for(int val:arr)
+                sb.append(val).append(' ');
+            sb.append('\n');
+            return;
+        }
+
+        for(int i=at;i<=N;i++)
+        {
+            arr[depth] = i;
+            dfs(i+1,depth+1);
+        }
+    }
+
+}
