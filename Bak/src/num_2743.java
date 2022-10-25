b6b864362b39fa7352f8cabf589ea7ec23443ed1
SangHwan <tkdghks4564@naver.com>
2022-10-25 23:18:34 +0900

    2743

---
+++ Bak/src/num_2743.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class num_2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String N = br.readLine();
+        System.out.println(N.length());
+    }
+}
+
