1491719b843119275347a348577a38f613b5bc01
SangHwan <tkdghks4564@naver.com>
2022-10-30 22:20:57 +0900

    5337

---
+++ Bak/src/num_5337.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class num_5337 {
+    public static void main(String[] args) throws IOException {
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
