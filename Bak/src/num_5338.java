5429011beaa4ce16afdec8144de6e06e45e0f643
SangHwan <tkdghks4564@naver.com>
2022-11-02 23:26:06 +0900

    5338

---
+++ Bak/src/num_5338.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class num_5338 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("       _.-;;-._");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-;;-._|");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-''-._|");
+    }
+}
