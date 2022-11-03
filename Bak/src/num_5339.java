d7c69c31b5fc781c394fc791aaaf51d8e71405fe
SangHwan <tkdghks4564@naver.com>
2022-11-03 19:48:50 +0900

    5339

---
+++ Bak/src/num_5339.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+public class num_5339 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("     /~\\");
+        System.out.println("    ( oo|");
+        System.out.println("    _\\=/_");
+        System.out.println("   /  _  \\");
+        System.out.println("  //|/.\\|\\\\");
+        System.out.println(" ||  \\ /  ||");
+        System.out.println("============");
+        System.out.println("|          |");
+        System.out.println("|          |");
+        System.out.println("|          |");
+    }
+}
