3289622e3f48e34559d78ec9176d4aefea5eb20b
SangHwan <tkdghks4564@naver.com>
2022-11-07 20:56:42 +0900

    7287

---
+++ Bak/src/num_7287.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class num_7287 {
+    public static void main(String[] args) throws IOException {
+        System.out.println(151);
+        System.out.println("tkdghks4564");
+    }
+}
